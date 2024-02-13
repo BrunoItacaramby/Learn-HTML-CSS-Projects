711e1ce355ea54d23a8a1854b3f35df9c65e66bf
BrunoItacaramby <brunobob69@hotmail.com>
2024-02-13 20:19:30 -0300

    Funcao de clicar para aparecer o menu dos apps 80% concluida

--- Yahoo/js/script.js
+++ Yahoo/js/script.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     var searchBar = document.getElementById('search-bar');
     var overlay = document.getElementById('overlay');
+    var app_button = document.getElementById('img-apps')
+    var app_window = document.getElementById('stepdown-window')
+
 
     // Exibir o overlay quando a barra de pesquisa ganhar foco
     searchBar.addEventListener('focus', function () {
         overlay.style.display = 'block';
         searchBar.style.boxShadow = 'none'
+    });
+
+    app_button.addEventListener('click', function () {
+        app_window.style.display = 'block';
     });
 
     // Esconder o overlay quando a barra de pesquisa perder o foco
