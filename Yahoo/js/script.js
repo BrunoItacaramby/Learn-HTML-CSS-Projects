ba60dee449e0af970a0ab199b6ba5dc613cdcb68
BrunoItacaramby <brunobob69@hotmail.com>
2024-02-13 22:29:24 -0300

    Agora a barra de pesquisa é limpa quando clica no X, adicionado cursor pointer onde ainda nao tinha

--- Yahoo/js/script.js
+++ Yahoo/js/script.js
@@ -3,21 +3,59 @@ document.addEventListener('DOMContentLoaded', function () {
     var overlay = document.getElementById('overlay');
     var app_button = document.getElementById('img-apps')
     var app_window = document.getElementById('stepdown-window')
+    var searchBar = document.getElementById('search-bar');
+    var x_button = document.getElementById('img-x')
 
 
-    // Exibir o overlay quando a barra de pesquisa ganhar foco
+
     searchBar.addEventListener('focus', function () {
         overlay.style.display = 'block';
         searchBar.style.boxShadow = 'none'
     });
 
-    app_button.addEventListener('click', function () {
-        app_window.style.display = 'block';
-    });
-
-    // Esconder o overlay quando a barra de pesquisa perder o foco
-    // Note que isso pode precisar de ajustes se você quiser manter o overlay enquanto interage com a barra de pesquisa
     searchBar.addEventListener('blur', function () {
         overlay.style.display = 'none';
     });
+
+    app_button.addEventListener('click', function (event) {
+        if(app_window.style.display === 'block') {
+            app_window.style.display = 'none';
+        }else {
+            app_window.style.display = 'block';
+        } 
+        event.stopPropagation();       
+    });
+
+    
+
+
+document.addEventListener('click', function (event) {
+    var isClickInsideAppWindow = app_window.contains(event.target);
+
+    if (!isClickInsideAppWindow && app_window.style.display === 'block') {
+        app_window.style.display = 'none';
+    }
+});
+
+searchBar.addEventListener('input', function () {
+    if (searchBar.value) {
+        x_button.style.display = 'block';
+    }
+});
+
+x_button.addEventListener('click', function () {   
+       
+        searchBar.focus()
+        searchBar.value = '';
+        x_button.style.display = 'none'
+
+});
+
+document.getElementById('img-x').addEventListener('mouseenter', function() {
+    this.src = './img/x-hover.png';
+});
+
+document.getElementById('img-x').addEventListener('mouseleave', function() {
+    this.src = './img/x.png';
+});
 });
