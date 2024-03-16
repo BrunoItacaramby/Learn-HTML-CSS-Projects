bf4af0a0d98d4571eee4e7aeed0f4f28d61f81d2
BrunoItacaramby <brunobob69@hotmail.com>
2024-03-16 20:05:14 -0300

    Agora tambem é mostrado no resultado qual é o tipo de IMC que corresponde com a pessoa

--- Calcule IMC/assets/js/script.js
+++ Calcule IMC/assets/js/script.js
@@ -1,7 +1,7 @@
 function meuEscopo () {
     const form = document.querySelector('.form')
     const dados = {}
-    let bg
+    let imcTexto
     const resultado = document.getElementById('resultado')
 
 
@@ -14,15 +14,52 @@ function meuEscopo () {
 
         const imc = (dados.peso/(dados.altura**2)).toFixed(1)
 
-        if(imc < 18.5 || imc > 30){
-            bg = 'rgb(236, 66, 66)' // vermelho
-        }else{
-            bg = 'rgb(66, 236, 117)' // verde
+        if(imc < 18.5){
+            imcTexto = 'Abaixo do peso'
+        }
+        else if(imc >= 18.5 && imc <= 24.9){
+            imcTexto = 'Peso normal'
+        }
+        else if(imc >= 25 && imc <= 29.9){
+            imcTexto = 'Sobrepeso'
+        }
+        else if(imc >= 30 && imc <= 34.9){
+            imcTexto = 'Obesidade grau 1'
+        }
+        else if(imc >= 35 && imc <= 39.9){
+            imcTexto = 'Obesidade grau 2'
+        }
+        else{
+            imcTexto = 'Obesidade grau 3'
         }
 
+        if(dados.peso == '' && dados.altura == ''){
+            resultado.innerHTML = `<h2>Preencha os campos para realizar o cálculo do IMC</h2>`
+            resultado.style.backgroundColor = 'rgb(236, 66, 66)' // vermelho
+        }
+        else if(dados.peso == ''){
+            resultado.innerHTML = `<h2>O campo "Peso" não pode estar em branco</h2>`
+            resultado.style.backgroundColor = 'rgb(236, 66, 66)' // vermelho
+        }
+        else if(dados.altura == ''){
+            resultado.innerHTML = `<h2>O campo "Altura" não pode estar em branco</h2>`
+            resultado.style.backgroundColor = 'rgb(236, 66, 66)' // vermelho
+        }
+        else if(imc < 18.5 || imc >= 30){
+            resultado.innerHTML = `<h2>Seu IMC é: ${imc} (${imcTexto})</h2>`
+            resultado.style.backgroundColor = 'rgb(236, 66, 66)' // vermelho
+        }
+        else if(imc > 25 && imc < 29.9){
+            resultado.innerHTML = `<h2>Seu IMC é: ${imc} (${imcTexto})</h2>`
+            resultado.style.backgroundColor = 'rgb(236, 154, 66)' // vermelho
+        }
+        else{
+            resultado.innerHTML = `<h2>Seu IMC é: ${imc} (${imcTexto})</h2>`
+            resultado.style.backgroundColor = 'rgb(66, 236, 117)' // verde
+        }
         resultado.style.display = 'block'
-        resultado.style.backgroundColor = `${bg}`
-        resultado.innerHTML = `<h2>Seu IMC é: ${imc}</h2>`
+        // resultado.style.backgroundColor = `${bg}`
+        // resultado.innerHTML = `<h2>Seu IMC é: ${imc}</h2>`
         
         console.log(imc)       
         
